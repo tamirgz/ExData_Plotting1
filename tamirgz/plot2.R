d4685fcf1933d95c57314780610cc273a0ad6516
Tamir Gazneli <gaznelit@marvell.com>
2015-03-06 20:01:31 +0200

    final modifications
    Change-Id: I36ba83c90b25ff6711931194ec6196bab09c8404

--- tamirgz/plot2.R
+++ tamirgz/plot2.R
@@ -3,11 +3,18 @@ d = read.table("household_power_consumption.txt",
                header = TRUE,
                sep=";",
                na.strings="?",
-               col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")) ## read the database file
+               col.names=c("Date","Time","Global_active_power",
+                           "Global_reactive_power","Voltage",
+                           "Global_intensity","Sub_metering_1",
+                           "Sub_metering_2","Sub_metering_3"))
 
-data <- d[d$Date=="1/2/2007" | d$Date=="2/2/2007",] ## extract only required dates
+## extract only required dates
+data <- d[d$Date=="1/2/2007" | d$Date=="2/2/2007",]
 dt <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
-plot(dt,as.numeric(data$Global_active_power),type="l",xlab="",ylab="Global Active Power (kilowatts)") ## plot the data
 
-dev.copy(png, file = "plot2.png", width=480,height=480) ## Copy my plot to a PNG file
+## plot the data
+plot(dt,as.numeric(data$Global_active_power),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+
+## Copy my plot to a PNG file
+dev.copy(png, file = "plot2.png", width=480,height=480) 
 dev.off() ## Don't forget to close the PNG device!
--- tamirgz/plot3.R
+++ tamirgz/plot3.R
@@ -3,14 +3,22 @@ d = read.table("household_power_consumption.txt",
                header = TRUE,
                sep=";",
                na.strings="?",
-               col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")) ## read the database file
+               col.names=c("Date","Time","Global_active_power",
+                           "Global_reactive_power","Voltage",
+                           "Global_intensity","Sub_metering_1",
+                           "Sub_metering_2","Sub_metering_3"))
 
-data <- d[d$Date=="1/2/2007" | d$Date=="2/2/2007",] ## extract only required dates
+## extract only required dates
+data <- d[d$Date=="1/2/2007" | d$Date=="2/2/2007",] 
 dt <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
-plot(dt,as.numeric(data$Sub_metering_1),type="l",xlab="",ylab="Energy sub metering") ## plot the data
+
+## plot the data
+plot(dt,as.numeric(data$Sub_metering_1),type="l",xlab="",ylab="Energy sub metering") 
 lines(dt,as.numeric(data$Sub_metering_2),col="red")
 lines(dt,as.numeric(data$Sub_metering_3),col="blue")
+## add legend
 legend("topright",lty=c(1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-dev.copy(png, file = "plot3.png", width=480,height=480) ## Copy my plot to a PNG file
+## Copy my plot to a PNG file
+dev.copy(png, file = "plot3.png", width=480,height=480) 
 dev.off() ## Don't forget to close the PNG device!
